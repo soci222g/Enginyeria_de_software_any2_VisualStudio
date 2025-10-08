9d2d38ec3215472be90901cf14581809fec5517f
Jordi Gratacos <jordi.gratacos@enti.cat>
2025-10-08 18:01:54 +0200

    update practicar dice game

--- practica Dice enguine/practica Dice enguine/main.cpp
+++ practica Dice enguine/practica Dice enguine/main.cpp
@@ -8,13 +8,7 @@
 
 void RallGame(int numOfFaces) {
 
-	int result = rand() % numOfFaces;
 
-	std::cout << "you roll a: " << result << "/" << numOfFaces << std::endl;
-
-	std::cout << "press eny key to continue " << std::endl;
-
-	_getch();
 
 }
 
